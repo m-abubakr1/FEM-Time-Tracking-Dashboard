cb3df0edb3171dbb0f048af37ea4232c6744a0ed
M. Abubakr <mabubakr429@gmail.com>
2025-02-25 22:03:54 +0500

    Fetch Data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function fetchData() {
   try {
     let response = await fetch("/data.json");
     let data = await response.json();
-    console.log(data);
+    console.log(data.timeframes[2]);
   } catch (error) {
     console.log("Error:", error);
   }
