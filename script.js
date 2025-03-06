ef77042f0ec2dd490c81cf2ac81e5ddc824143ed
M. Abubakr <mabubakr429@gmail.com>
2025-03-06 17:57:17 +0500

    Update Script.js (Fetching data)

--- script.js
+++ script.js
@@ -13,15 +13,3 @@ fetch("./data.json")
   .catch((error) => {
     console.error("Error Fetching data: ", error);
   });
-
-// async function fetchData() {
-//   try {
-//     let response = await fetch("/data.json");
-//     let data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.log("Error:", error);
-//   }
-// }
-
-// fetchData();
